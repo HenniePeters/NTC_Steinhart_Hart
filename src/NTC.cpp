1947495ae41061b635a04b39f75d744031f1059d
Hennie Peters <58438328+HenniePeters@users.noreply.github.com>
2023-02-28 14:25:58 +0100

    Add files via upload

--- src/NTC.cpp
+++ src/NTC.cpp
@@ -17,8 +17,8 @@ NTC::NTC( const double a, const double b, const double c ) {
     Scale = SCALE_C;
     bHyst = false;
     Rs = 0.0;
-    Adc = 0;
-    MaxAdc = 1023;
+    Adc = 0.0;
+    MaxAdc = 1023.0;
     Correct = 0.0;
     Equation = EQUATION_3;
     SetDecimals( 1 );
@@ -26,17 +26,17 @@ NTC::NTC( const double a, const double b, const double c ) {
 //==========================================================================================
 void NTC::SetDecimals( uint8_t decimals ) {
     decim = decimals;
-    if ( decimals == 0 ) {
+    if( decimals == 0 ) {
         strcpy( fmt, "%d" );
     } else {
         sprintf( fmt, "%cd.%c0%dd", '\%', '\%', decim );
     }
     Factor = pow( 10.0, decim );
     Rounder = 1.0 / pow( 10.0, decim ) / 2.0;
-    if ( bHyst ) {
-        Hyster = 1;
+    if( bHyst ) {
+        Hyster = 1.0;
     } else {
-        Hyster = 0;
+        Hyster = 0.0;
     }
 }
 //==========================================================================================
@@ -51,11 +51,11 @@ char *NTC::c_str( char *buf, uint8_t scale ) {
 char *NTC::c_str( char *buf ) {
     char *p = buf;
     double f = GetTemperature();
-    if ( f <= 0 - Rounder ) {
+    if( f <= 0.0 - Rounder ) {
         *p++ = '-';
     }
     f = abs( f ) + Rounder;
-    sprintf( p, fmt, (int)f, (int)( f * Factor ) % (int)Factor );
+    sprintf( p, fmt, ( int )f, ( int )( f * Factor ) % ( int )Factor );
     return buf;
 }
 //==========================================================================================
@@ -65,11 +65,11 @@ void NTC::SetCorrection( double correction ) {
 //==========================================================================================
 double NTC::GetBoilingPointWater( double mBar ) {
     double centigrade = -0.0000104846 * pow( mBar, 2.0 ) + 0.04890953 * mBar + 61.206652;
-    if ( Scale == SCALE_C ) {
+    if( Scale == SCALE_C ) {
         return centigrade;
-    } else if ( Scale == SCALE_F ) {
+    } else if( Scale == SCALE_F ) {
         return centigrade * 9.0 / 5.0 + 32.0;
-    } else if ( Scale == SCALE_K ) {
+    } else if( Scale == SCALE_K ) {
         return centigrade + 273.15;
     } else {
         assert( Scale == SCALE_C || Scale == SCALE_F || Scale == SCALE_K );
@@ -82,24 +82,23 @@ void NTC::SetScale( uint8_t scale ) {
 }
 //==========================================================================================
 void NTC::SetMaxAdc( uint16_t max ) {
-    MaxAdc = (double)max;
+    MaxAdc = ( double )max;
 }
 //==========================================================================================
 void NTC::SetValueAdc( uint16_t value ) {
     static double curr_pivot = 0.0;
     static double prev_pivot = 0.0;
     static double prev_pos = 0.0;
-    if ( ( bHyst && value < prev_pos && dir == 1 ) ||
-         ( bHyst && value > prev_pos && dir == -1 ) ) { // direction has changed
+    if( ( bHyst && value < prev_pos && dir == 1 ) ||
+        ( bHyst && value > prev_pos && dir == -1 ) ) { // direction has changed
         dir = 0 - dir;
-        if ( fabs( fabs( curr_pivot ) - fabs( value ) ) <
-             Hyster ) {              // direction has changed but value change is within 'Hyster' from
+        if( fabs( fabs( curr_pivot ) - fabs( value ) ) <
+                Hyster ) {              // direction has changed but value change is within 'Hyster' from
             curr_pivot = prev_pivot; // the last pivot, reuse the previous pivot and ignore current position
         } else {
             prev_pivot = curr_pivot; // direction has changed, save the current pivot and set the new pivot
             curr_pivot = value;
-            if ( fabs( fabs( value ) - fabs( prev_pos ) ) >
-                 Hyster ) { // skip value if turning point is within 'Hyster' from last position
+            if( fabs( fabs( value ) - fabs( prev_pos ) ) > Hyster ) {  // skip value if turning point is within 'Hyster' from last position
                 Adc = value;
             }
         }
@@ -122,7 +121,7 @@ void NTC::SetHysteresis( double Hysteresis ) {
 }
 //==========================================================================================
 void NTC::SetSeriesResistor( long Resistance ) {
-    Rs = (double)Resistance; // Rs = series resistor
+    Rs = ( double )Resistance; // Rs = series resistor
 }
 //==========================================================================================
 double NTC::GetTemperature( uint8_t scale ) {
@@ -134,31 +133,31 @@ double NTC::GetTemperature( uint8_t scale ) {
 }
 //==========================================================================================
 double NTC::GetTemperature( void ) {
-    if ( Method == NTC_TOP ) {
+    if( Method == NTC_TOP ) {
         Rt = Rs * ( MaxAdc - Adc ) / Adc; // Rt = thermistor resistance
-    } else if ( Method == NTC_BOTTOM ) {
+    } else if( Method == NTC_BOTTOM ) {
         Rt = Rs * Adc / ( MaxAdc - Adc );
     } else {
         assert( ( Method == NTC_TOP ) || ( Method == NTC_BOTTOM ) );
     }
 
     double Kelvin = 0.0;
-    if ( Equation == EQUATION_3 ) {
+    if( Equation == EQUATION_3 ) {
         Kelvin = 1.0 / ( A + B * LN( Rt ) + C * pow( LN( Rt ), 3.0 ) );
-    } else if ( Equation == EQUATION_4 ) {
+    } else if( Equation == EQUATION_4 ) {
         Kelvin = 1 / ( A + B * LN( Rt ) + C * pow( LN( Rt ), 2 ) + D * pow( LN( Rt ), 3 ) );
     } else {
         assert( Equation == EQUATION_3 || Equation == EQUATION_4 );
     }
 
-    if ( Scale == SCALE_C ) {
+    if( Scale == SCALE_C ) {
         double Centigrade = Kelvin - 273.15;
         return Centigrade + Correct;
-    } else if ( Scale == SCALE_F ) {
+    } else if( Scale == SCALE_F ) {
         double Centigrade = Kelvin - 273.15;
         Centigrade += Correct;
         return Centigrade * 9.0 / 5.0 + 32.0;
-    } else if ( Scale == SCALE_K ) {
+    } else if( Scale == SCALE_K ) {
         return Kelvin + Correct;
     } else {
         assert( Scale == SCALE_C || Scale == SCALE_F || Scale == SCALE_K );
